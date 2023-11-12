89f1f30ca07416a41cbddd952213680b2d929f8a
tranVyTDT <40004136+tranVyTDT@users.noreply.github.com>
2023-11-12 23:53:57 +0700

    final

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=postgres
+export POSTGRES_PASSWORD=postgresadmin
+export POSTGRES_HOST=postgres.csli3utjeptr.ap-southeast-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=vyx-s3-3
+export AWS_REGION=ap-southeast-1
+export AWS_PROFILE=default
+export JWT_SECRET=testing
+export URL=http://localhost:8100
